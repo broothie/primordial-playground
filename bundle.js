de96de643f4e26ac71de4e427ef82f596a70e475
boothandrewd <boothandrewd@gmail.com>
2017-08-03 23:25:11 -0700

    Add homage color schemes

--- bundle.js
+++ bundle.js
@@ -784,9 +784,9 @@ exports.default = {
   'Flappy Bird': ['#4EC0CA', '#A5C55C', '#FE3800', '#F6B833'],
   'Electric': ['#B3B3B3', '#FFFF66', '#00FFFF', '#008080'],
   'Swayed': ['#6F00CA', '#800040', '#000000', '#804000'],
-  'Rick and Morty': [],
-  'App Academy': [],
-  'UB': []
+  'Rick and Morty': ['#B5E4F9', '#FDFDFD', '#824816', '#FBFF6E'],
+  'App Academy': ['#FFFFFF', '#FFFFFF', '#FF3850', '#FF0000'],
+  'UB': ['#005B9D', '#62A7D2', '#000000', '#FFFFFF']
 };
 
 /***/ }),
